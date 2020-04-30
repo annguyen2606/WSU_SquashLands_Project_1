4c747944f93ebadcc21c622a845d9b767cb135ad
An Nguyen <annguyen260691@gmail.com>
2020-04-30 18:23:01 +1000

    add back button for request screen

--- SquashLandsWSUProject/app/src/main/java/com/example/squashlandswsuproject/RequestSongFragment.kt
+++ SquashLandsWSUProject/app/src/main/java/com/example/squashlandswsuproject/RequestSongFragment.kt
@@ -6,14 +6,21 @@ import android.widget.Toast
 import androidx.fragment.app.Fragment
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
+import kotlinx.android.synthetic.main.fragment_request_songs.*
 
 class RequestSongFragment : Fragment(R.layout.fragment_request_songs){
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
         val recyclerViewRequest = view.findViewById<RecyclerView>(R.id.recyclerViewRequest)
-        var adapter = CustomRequestRecyclerViewAdapter(MainActivity.request,view.context)
+        val adapter = CustomRequestRecyclerViewAdapter(MainActivity.request,view.context)
 
         recyclerViewRequest.adapter = adapter
         recyclerViewRequest.layoutManager = LinearLayoutManager(view.context, RecyclerView.VERTICAL, false)
+        buttonRequestBack.setOnClickListener {
+            val ft = this.fragmentManager?.beginTransaction()
+            ft?.remove(this)
+            ft?.replace(R.id.fragment_holder, HomeFragment(), "fragment_home")
+            ft?.commit()
+        }
     }
 }
